04907bffe12da344c2ea2a1aa8f3f99936db52fd
Geoff deRosenroll <geoffderosenroll@gmail.com>
2023-05-02 21:10:41 -0700

    Additional env vars to guide flag setting

--- c/config/discover.ml
+++ c/config/discover.ml
@@ -1,32 +1,29 @@
 let unix_usual_cuda = "/usr/local/cuda/lib64"
 let maybe_arch_cuda = "/opt/cuda/lib64"
-(* let base_flags = [ "-lstdc++"; "-lassimp"; "-fopenmp" ] *)
+let base_flags = [ "-lstdc++"; "-lassimp" ]
+let cuda dir = [ Printf.sprintf "-L%s" dir; "-lcuda"; "-lcudart" ]
 
-let base_flags = [ "-lstdc++"; "-lassimp"; "-ltbb" ]
-let cuda_flags dir = [ Printf.sprintf "-L%s" dir; "-lcuda"; "-lcudart" ]
+let par_flags =
+  match Option.map String.lowercase_ascii @@ Sys.getenv_opt "OMANIFOLD_PAR" with
+  | Some "omp" -> [ "-fopenmp" ]
+  | Some "none" -> []
+  | _ -> [ "-ltbb" ]
 
-let use_cuda, flags =
-  (* TODO:
-     - is the runtime always at the usual unix location on macos?
-     - windows support (seems like the directories are always versioned there,
-       so it will need to be more sophisticated than the current naive and
-       inflexible setup)
-       https://docs.nvidia.com/cuda/cuda-installation-guide-microsoft-windows/#installing-cuda-development-tools
-       For example: this guide says the default is:
-       C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v12.0 *)
+let use_cuda, cuda_flags =
   match Option.map String.lowercase_ascii @@ Sys.getenv_opt "OMANIFOLD_USE_CUDA" with
   | Some ("on" | "true" | "yes") ->
-    let libs =
+    if Sys.os_type = "Unix"
+    then
       if Sys.file_exists unix_usual_cuda && Sys.is_directory unix_usual_cuda
-      then cuda_flags unix_usual_cuda
+      then true, cuda unix_usual_cuda
       else if Sys.file_exists maybe_arch_cuda && Sys.is_directory maybe_arch_cuda
-      then cuda_flags maybe_arch_cuda
-      else []
-    in
-    ( match libs with
-      | [] -> false, base_flags
-      | libs -> true, base_flags @ libs )
-  | _ -> false, base_flags
+      then true, cuda maybe_arch_cuda
+      else false, []
+    else (
+      match Option.map String.lowercase_ascii @@ Sys.getenv_opt "OMANIFOLD_CUDA_PATH" with
+      | Some path -> true, cuda path
+      | None -> false, [] )
+  | _ -> false, []
 
 let () =
   Out_channel.with_open_bin "use_cuda" (fun oc ->
@@ -34,4 +31,4 @@ let () =
 
 let () =
   Out_channel.with_open_bin "c_library_flags" (fun oc ->
-    List.iter (Printf.fprintf oc "%s\n") flags )
+    List.iter (List.iter (Printf.fprintf oc "%s\n")) [ base_flags; par_flags; cuda_flags ] )
